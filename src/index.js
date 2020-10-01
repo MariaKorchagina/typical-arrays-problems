5a180f62a4d7e5829be2021bdb795ceb18190920
Maria Korchagina <kobysheva_maria@mail.ru>
2020-10-02 00:51:27 +0400

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+if (array && array.length) {
+   // not empty
+return Math.min(...array);
+} else {
+   // empty
   return 0;
+}
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+if (array && array.length) {
+   // not empty
+ return Math.max(...array);
+} else {
+    // empty
+   return 0;
+ }
+ }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+if (array && array.length) {
+   // not empty
+ return array.reduce((a, b) => (a + b)) / array.length;
+} else {
+    // empty
+   return 0;
+ }
+ }
